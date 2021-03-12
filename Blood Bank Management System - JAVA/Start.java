fe1adcf7e39d730a713545da2a540c74e4a97b12
arnabb38 <arnabbasak896@gmail.com>
2021-03-12 13:06:36 +0600

    updated project structure and removed unnecessary classes

--- Blood Bank Management System - JAVA/Start.java
+++
@@ -1,8 +0,0 @@
-public class Start
-{
-	public static void main(String args[]) throws Exception
-	{
-		Homee h = new Homee();
-		h.setVisible(true);
-	}
-}
---
+++ bloodbankManagementSystem/src/com/arnabb/bbms/Start.java
@@ -0,0 +1,12 @@
+package com.arnabb.bbms;
+
+import com.arnabb.bbms.Home.Homee;
+
+public class Start
+{
+	public static void main(String args[]) throws Exception
+	{
+		Homee h = new Homee();
+		h.setVisible(true);
+	}
+}
